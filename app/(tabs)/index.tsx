11d43533fc6072e584046b4061ba2769ee078a00
Justyna <justyna.sienkiewicz93@gmail.com>
2025-03-19 21:10:32 +0100

    clear example files

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,61 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Text, View, StyleSheet, Button } from 'react-native'
+import { useAuth0 } from 'react-native-auth0'
+import axios from 'axios'
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+export default function Index() {
+	const { authorize, getCredentials, clearSession, user } = useAuth0()
+	const apiUrl = process.env.EXPO_PUBLIC_API_URL
 
-export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+	const signIn = async () => {
+		try {
+			await authorize()
+		} catch (e) {
+			console.log(e)
+		}
+	}
+
+	const signOut = async () => {
+		try {
+			await clearSession()
+		} catch (e) {
+			console.log(e)
+		}
+	}
+
+	const callApi = async () => {
+		const credentials = await getCredentials()
+		axios
+			.get(apiUrl!, {
+				headers: {
+					Authorization: `Bearer ${credentials?.idToken}`,
+				},
+				timeout: 1000,
+			})
+			.then(res => {
+				if (res.status !== 200) console.log(res)
+				console.log(res.data)
+			})
+			.catch(err => console.log(err))
+	}
+
+	return (
+		<View style={styles.container}>
+			<Text>User: {user?.nickname}</Text>
+			<Button onPress={signIn} title='Sign In' />
+			<Button onPress={callApi} title='Call API' />
+			<Button onPress={signOut} title='Sign Out' />
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		gap: 20,
+	},
+	warn: {
+		backgroundColor: 'red',
+	},
+})
